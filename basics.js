0f650e0a94de7481082af72b56b916ec7ae52065
JSlovak <jasmine.slovak@gmail.com>
2017-03-13 16:01:16 -1000

    Whoops... I got ahead of myself, committing exercises 1 - 3.

--- basics.js
+++ basics.js
@@ -1,18 +1,29 @@
 /* Create a `myName` variable and assign it a String value */
 
+var myName = "Princess Leia";
+
 /* Create a `person` variable and give it 2 properties,
  * `name`, assign it the same name as before,
  * as well as an `age` (number);
  */
 
+ var person = {
+  name: "Princess Leia",
+  age: 22
+ };
+
 /* Create a variable called `canDrive`,
  * if it should be true if your person object is at least 16 years old
  */
+
+ var canDrive = (age => 21);
 
 /* Create a function called `greet`,
  * it should take a 1 parameter, `name`
  * and it should print "Hello, my name is {name}"
  */
+
+
 
 /* Create an array called `dataTypes` with atleast 1 of every data type;
  * (there are 6 different data types);
