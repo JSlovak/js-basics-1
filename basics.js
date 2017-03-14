032d4b458d11201925a3355be92f4a13151cf3e0
JSlovak <jasmine.slovak@gmail.com>
2017-03-13 16:30:11 -1000

    Completed Exercise dog object literal

--- basics.js
+++ basics.js
@@ -40,3 +40,11 @@ var dataTypes = [true, null, undefined, "String", 13, {exercise: "js basics"}, [
  * it should have a `bark` function that makes your dog bark!
  * It should also have a name attribute with the value of 'Spot'
  */
+
+var dog = {
+  name: "Spot",
+  bark: function(){
+    console.log (dog.name + " says 'Ruff'.");
+    }
+  };
+
